c40a934a2cbb5e6b8310313fecb156a8b356382f
te <git@fetchte.com>
2025-09-02 10:14:06 -0600

    refactor(src): extract env detection, optimize ansi escape
    - move environment variable and argv parsing to `constants.ts`
    - relocate color space detection to `constants.ts`
    - add `simpleEscape` option for faster, non-nested ansi escaping
    - streamline color definition and lookup with pre-defined maps
    - rename internal types for improved clarity

--- src/index.ts
+++ src/index.ts
@@ -1,162 +1,11 @@
+import {
+  COLOR_SPACE,
+  ESC,
+  RESET,
+} from './constants.ts';
 
 type EmptyObject = Record<never, never>;
 type StrNum = string | number;
-
-
-/**
- * color support level with cache control (negative=disable caching)
- * @NOTE negatives == positive, but a neg disables caching to force a plugin to always re-run
- * @nsole param:5
- * @default 3
- * @see {@link https://nodejs.org/api/cli.html#force_color1-2-3|Node.js FORCE_COLOR docs}
- */
-export type ColorSpace = -4 | -3 | -2 | -1 | 0 | 1 | 2 | 3;
-
-const ESC = '\x1B';
-const RESET = ESC + '[0m';
-
-// polly - just  in case
-const GLOBAL_THIS = typeof globalThis !== 'undefined' ? globalThis : {} as typeof globalThis;
-
-/**
- * env variables (pure/iffe wrapped so we can shake the tree)
- * @see {@link https://nodejs.org/api/process.html#processenv|Node.js env docs}
- */
-const PROC_ENV = /* @__PURE__ */ (() => typeof process === 'undefined'
-  ? {}
-  /** deno {@link https://docs.deno.com/runtime/reference/env_variables/#built-in-deno.env} */
-  : (!(GLOBAL_THIS as never)?.['Deno']
-      ? process['env']
-      // deno requires permission to access env: --allow-env
-      : (() => {
-          try {
-            // if --allow-all is used, no toObject (like --allow-env), use default
-            // @ts-expect-error deno perms
-            return process['env']?.toObject ? process['env']?.toObject() : process['env'];
-          } catch (_err) { /* ignore */ }
-          return {};
-        })())
-)();
-
-
-/**
- * command-line arguments (pure/iffe wrapped so we can shake the tree)
- * @see {@link https://nodejs.org/api/process.html#processargv|Node.js process.argv docs}
- */
-const ARGV: string[] = /* @__PURE__ */ (() =>
-  typeof process === 'undefined'
-    /** quickJs {@link https://bellard.org/quickjs/quickjs.html#Global-objects} */
-    ? typeof scriptArgs !== 'undefined'
-      ? scriptArgs
-      : []
-    : ((GLOBAL_THIS as never)?.['Deno']
-        // if --allow-all is used, no args (like --allow-env), use default
-        // @ts-expect-error deno uses 'args' rather than 'argv'
-        ? (process['args']?.length ? process['args'] : process['argv'])
-        : process['argv']
-      ) ?? []
-)();
-
-
-/**
- * checks for presence of key (flags) in argv
- * @example --example -flag
- * @param  {string | string[]} keys - argv key/id (--key, -key)
- * @param  {string[]} [argv=ARGV]   - command-line argv
- * @return {boolean}
- */
-const hasArgvFlags = (keys: string | string[], argv = ARGV): boolean => !argv?.length
-  ? false
-  : !!([keys].flat().find(key =>
-      (new RegExp(`(^|[^\\S])(?:--|-)${key}(=|\\s|$)`, 'i')).test(argv.join(' '))));
-
-
-/**
- * gets any* reasonable argv option value by key (returns null for flags, must have value)
- * @example --key=value --key="value" --key="val ue" --key "val ue" (-- | - | "" | '')
- * @note   un-reasonable values: non-escaped multi-lines; malformed keys; mismatching quotes
- * @param  {string} key           - argv key/id (--key, -key)
- * @param  {string[]} [argv=ARGV] - command-line argv
- * @return {null | string}
- */
-const getArgvOption = (key: string, argv = ARGV) => !argv?.length
-  ? null
-  : ` ${argv.join(' ')} `.match(
-    // eslint-disable-next-line @stylistic/max-len
-    new RegExp(`(^|[\\s])(?:--|-)${key}(?:=|\\s+)(['"]?)([^-\\s][^'"-\\s]*?|(?:\\D?[^-].*?))\\2(?:\\s|$)`, 'i'),
-    // normalize empty single double quotes pairs:  --key="" | --key=''
-  )?.[3]?.replace(/^(''|"")$/, '') ?? null;
-
-
-/**
- * snags an argv by key/id including flags (a flag value is '', null-check-it)
- * @example
- *   --nsole-lvl=0 --nsole_lvl=0 -nsole-lvl=0 -nsole_lvl=0
- *   --nsole-lvl 0 --nsole_lvl 0 -nsole-lvl 0 -nsole_lvl 0
- *   --NSOLE-LVL=0 --NSOLE_LVL=0 -NSOLE-LVL=0 -NSOLE_LVL=0
- *   --NSOLE-LVL 0 --NSOLE_LVL 0 -NSOLE-LVL 0 -NSOLE_LVL 0
- * @param  {string} key         - argv key/id (--key=value)
- * @param  {string[]} [argv=ARGV]
- * @return {null | string | ''}
- */
-const getArgv = (key: string, argv = ARGV): null | string | '' => !argv?.length
-  ? null
-  : hasArgvFlags(key, argv)
-    ? (getArgvOption(key, argv) ?? '')
-    // @meh, fix for when (i) use hyphens in argvs; NSOLE_LVL -> NSOLE-LVL
-    : hasArgvFlags(key.replaceAll('_', '-'), argv)
-      ? (getArgvOption(key.replaceAll('_', '-'), argv) ?? '')
-      : null;
-
-/**
- * gets env var key/value pairs; priority: argv > env > globalThis > null
- * @param  {string} key                  - key (argv not case sensitive, procEnv/globalThis is)
- * @param  {string[]} [argv=ARGV]        - process['argv']
- * @param  {NodeJS.ProcessEnv} [procEnv=PROC_ENV] - process['env']
- * @param  {globalThis} [gthis=globalThis] - globalThis
- */
-const getEnvVar = (key: string, argv = ARGV, procEnv = PROC_ENV, gthis = GLOBAL_THIS as never) =>
-  getArgv(key, argv)
-    ?? ((key in procEnv)
-      ? procEnv[key]
-      : (key in gthis)
-          ? gthis[key]
-          : null) ?? null;
-
-/**
- * COLOR_SPACE - covers all reasonable and most un-reasonable cases
- * @implements nodejs.org/api/cli.html#force_color1-2-3
- *             0=no-color, 1=16, 2=256, 3=true-color
- */
-const COLOR_SPACE: ColorSpace = /* @__PURE__ */ (() => {
-  try {
-    const forceColor = getEnvVar('FORCE_COLOR');
-    // force colors
-    if (forceColor === '' || (forceColor && !isNaN(Number(forceColor)) && Number(forceColor) > 0)) {
-      return 1;
-    }
-    // no proc
-    if (typeof process === 'undefined') { return 0; }
-    // no color var
-    if (getEnvVar('NO_COLOR') !== null
-      || getEnvVar('NODE_DISABLE_COLORS') !== null
-      || (/-mono|dumb/i).test(PROC_ENV['TERM'] ?? '')) { return 0; }
-    // no tty
-    if (!(!!process.stdout?.isTTY
-      || (!!PROC_ENV.PM2_HOME && !!PROC_ENV.pm_id)
-      || ((GLOBAL_THIS as never)?.['Deno']
-        ? (GLOBAL_THIS as any)?.['Deno']?.isatty(1)
-        : !!process.stdout?.isTTY))) { return 0; }
-    // cli
-    if ((/^(false|never|no|0)$/i).test(`${getArgvOption('color') ?? ''}`.trim())) {
-      return 0;
-    }
-    return 1;
-  } catch (_err) { /* ignore */ }
-  // catch/error/fallthrough; plays it safe with no colors
-  return 0;
-})();
-
 
 // fg=foreground
 // bg=background
@@ -168,41 +17,45 @@ const COLOR_SPACE: ColorSpace = /* @__PURE__ */ (() => {
 type StyleKeys = 'fg' | 'bg' | 'ft' | 'ue' | 'ie' | 're' | 'pr';
 type AnsiCodeTuple = [on: number, off: number, isCustom?: number];
 type StyleState = Partial<Record<StyleKeys, AnsiCodeTuple>>;
-type StainBaseColors = 'black' | 'blue' | 'cyan' | 'green' | 'purple' | 'red' | 'white' | 'yellow';
-export type StainColors = StainBaseColors | `i${StainBaseColors}`;
-// Generate XtermKeys from x0 to x255
-type GenerateXtermKeys<N extends number, Acc extends string[] = []> =
+type StainBase = 'black' | 'blue' | 'cyan' | 'green' | 'purple' | 'red' | 'white' | 'yellow';
+export type StainAnsi = StainBase | `i${StainBase}`;
+
+// generate xterms: x0 to x255
+type XtermKeysFactory<N extends number, Acc extends string[] = []> =
   Acc['length'] extends N
     ? Acc[number]
-    : GenerateXtermKeys<N, [...Acc, `x${Acc['length']}`]>;
-type XtermKeys = GenerateXtermKeys<256>;
-type CustomAnsiStainer<
+    : XtermKeysFactory<N, [...Acc, `x${Acc['length']}`]>;
+export type StainXterm = XtermKeysFactory<256>;
+
+export type Stain<
   C extends Record<string, number> = EmptyObject,
   X extends boolean = false,
 > =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ((...args: any[])=> string) &
   {
-    bg: CustomAnsiStainer<C, X>;
-    bold: CustomAnsiStainer<C, X>;
-    dim: CustomAnsiStainer<C, X>;
-    normal: CustomAnsiStainer<C, X>;
-    reset: CustomAnsiStainer<C, X>;
-    underline: CustomAnsiStainer<C, X>;
-    inverse: CustomAnsiStainer<C, X>;
+    bg: Stain<C, X>;
+    bold: Stain<C, X>;
+    dim: Stain<C, X>;
+    normal: Stain<C, X>;
+    reset: Stain<C, X>;
+    underline: Stain<C, X>;
+    inverse: Stain<C, X>;
   } &
   // built-in named colors
-  { [K in StainColors]: CustomAnsiStainer<C, X> } &
+  { [K in StainAnsi]: Stain<C, X> } &
   // xterm colors if enabled
-  (X extends true ? { [K in XtermKeys]: CustomAnsiStainer<C, X> } : EmptyObject) &
+  (X extends true ? { [K in StainXterm]: Stain<C, X> } : EmptyObject) &
   // custom colors from opts.colors
-  (keyof C extends never ? EmptyObject : { [K in keyof C]: CustomAnsiStainer<C, X> });
+  (keyof C extends never ? EmptyObject : { [K in keyof C]: Stain<C, X> });
 
-
-type StainOpts<C extends Record<string, number> = EmptyObject> = {
-  format?: (...args: unknown[])=> string;
+export type StainOpts<C extends Record<string, number> = EmptyObject> = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  format?: (...args: any[])=> string;
   noColor?: boolean;
   xterm?: boolean;
   colors?: C;
+  simpleEscape?: boolean;
 };
 
 
@@ -249,7 +102,8 @@ const escStain = (str: string, open: StrNum, close: StrNum, fontReplace?: StrNum
     if (isReset) {
       parts.push(RESET);
       resetCount++;
-      // after every second reset restore style - handles: stain.red(`text ${stain.reset('reset')} text`)
+      // after every second reset restore style
+      // e.g: stain.red(`text ${stain.reset('reset')} text`)
       if (resetCount % 2 === 0) { parts.push(openStr); }
       cursor = idx + RESET.length;
       continue;
@@ -278,6 +132,24 @@ const escStain = (str: string, open: StrNum, close: StrNum, fontReplace?: StrNum
   return openStr + parts.join('') + closeStr;
 };
 
+/**
+ * simple escape that doesn't handle nested ansi; 1.5-4x+ faster depending on style complexity
+ * nested example: stain.red.bold('Bold ' + stain.green.normal('normal') + ' Bold')
+ * @param  {string} str
+ * @param  {StrNum} open
+ * @param  {StrNum} close
+ * @param  {StrNum} _fontReplace
+ * @return {[type]}
+ */
+const escStainSimple = (str: string, open: StrNum, close: StrNum, _fontReplace?: StrNum) =>
+  `${ESC}[${open}m` + (typeof str !== 'string' ? String(str) : str) `${ESC}[${close}m`;
+
+
+const COLOR_DEF = {
+  black:30, iblack:90, red:31, ired:91, green:32, igreen:92, yellow:33, iyellow:93, blue:34, iblue:94, purple:35, ipurple:95, cyan:36, icyan:96, white:37, iwhite:97,
+};
+const XTERM_DEF = Object.fromEntries(Array(256).fill(0).map((_v, i) => [`x${i}`, i] as [string, number]));
+const COLOR_ALL = {...COLOR_DEF, ...XTERM_DEF};
 
 /**
  * a nice node typed color api, that isn't "slow" at
@@ -285,13 +157,13 @@ const escStain = (str: string, open: StrNum, close: StrNum, fontReplace?: StrNum
  */
 function createStain<C extends Record<string, number> = EmptyObject>(
   opts?: StainOpts<C> & { xterm?: false }
-): CustomAnsiStainer<C, false>;
+): Stain<C, false>;
 function createStain<C extends Record<string, number> = EmptyObject>(
   opts: StainOpts<C> & { xterm: true }
-): CustomAnsiStainer<C, true>;
+): Stain<C, true>;
 function createStain<C extends Record<string, number> = EmptyObject>(
   opts: StainOpts<C> = {},
-): CustomAnsiStainer<C, boolean> {
+): Stain<C, boolean> {
   const {
     colors,
     xterm,
@@ -299,41 +171,30 @@ function createStain<C extends Record<string, number> = EmptyObject>(
       ? args.join(' ')
       : typeof args[0] !== 'string' ? JSON.stringify(args[0]) : args[0],
     noColor = COLOR_SPACE === 0,
+    simpleEscape = false,
   } = opts;
-  const colorsAll: Record<string, number> = {};
-  const colorsDef: null | number[] = colors || xterm ? [] : null;
-  for (const [k, v] of Object.entries({
-    black: 30, red: 31, green: 32, yellow: 33, blue: 34, purple: 35, cyan: 36, white: 37,
-  })) {
-    colorsAll[k] = v;
-    colorsAll[`i${k}`] = v + 60;
-    // add default colors
-    colorsDef && [v, v + 60].forEach(i => {
-      colorsDef.push(i);
-      colorsDef.push(i + 10); // intense
-    });
-  }
-  // tracks 8-bit palette for escaping, not elegant, but works, todo replace with maths
-  const eBitColor: Record<string, number> = {};
-  const customColor = ([k, v]: [k: string, v: number]) => {
-    colorsAll[k] = v;
-    eBitColor[k] = v;
-  };
-  // add xterm 255 colors
-  xterm && Array(256).fill(0).map((_v, i) => [`x${i}`, i] as [string, number])
-    .forEach(customColor);
-  // add custom defined colors
-  colors && Object.entries(colors).forEach(customColor);
+
+  const colorAll: Record<string, number> = colors
+    ? {...(xterm ? COLOR_ALL : COLOR_DEF), ...colors}
+    : (xterm ? COLOR_ALL : COLOR_DEF);
 
   // provies the same api, but doesn't add color, and much, much, much, faster
   if (noColor) {
-    // avoids poluting proto on passed in utils.format
-    const plain = opts?.format ? (...args: unknown[]) => format(...args) : format;
-    const chain = Object.keys(colorsAll).concat(['bg', 'bold', 'dim', 'normal', 'reset', 'underline']);
-    // @ts-expect-error not typable
-    for (const prop of chain) { plain[prop] = plain; }
-    return plain as CustomAnsiStainer<C, boolean>;
+    // wrapped to avoid poluting the proto on passed in format
+    const ncFormat = opts?.format ? (...args: unknown[]) => format(...args) : format;
+    for (const prop of Object.keys(colorAll)
+      .concat(['bg', 'bold', 'dim', 'normal', 'reset', 'underline'])) {
+      // @ts-expect-error not typable
+      ncFormat[prop] = ncFormat;
+    }
+    return ncFormat as Stain<C, boolean>;
   }
+  const escFn = simpleEscape ? escStainSimple : escStain;
+  // xterm look-up map to determin if xterm
+  // @see {@link https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit}
+  const color256: Record<string, number> = colors
+    ? (xterm ? {...XTERM_DEF, ...colors} : colors)
+    : (xterm ? XTERM_DEF : {});
 
   // our main stain
   const colorProxy = (cur: StyleState = {}) =>
@@ -341,19 +202,19 @@ function createStain<C extends Record<string, number> = EmptyObject>(
       get: (_target, prop: string) => {
         const nxt = {...cur};
         const fg = nxt.fg;
-        if (colorsAll[prop] !== undefined) { // xterm logic -----------------------
-          // need to keep previous for bg logic
+        if (colorAll[prop] !== undefined) { // xterm logic
+          // keeps track of previous for bg logic
           nxt.pr = [
-            fg?.[0] ?? colorsAll[prop],
+            fg?.[0] ?? colorAll[prop],
             39,
-            fg?.[2] ?? (eBitColor[prop] !== undefined ? 1 : 0),
+            fg?.[2] ?? (color256[prop] !== undefined ? 1 : 0),
           ];
-          nxt.fg = [colorsAll[prop], 39, eBitColor[prop] !== undefined ? 1 : 0];
-        } else if (prop === 'bg' && fg) { // bg logic --------------------------
+          nxt.fg = [colorAll[prop], 39, color256[prop] !== undefined ? 1 : 0];
+        } else if (prop === 'bg' && fg) { // bg/fg logic
           nxt.bg = [fg[0] + (fg[2] || fg[1] === 49 ? 0 : 10), 49, fg[2]] as AnsiCodeTuple;
           // @ts-expect-error to make this type easier we ignore undefined
           if (nxt.pr?.[1] === 39) { nxt.fg = fg[0] === nxt.pr[0] ? undefined : [...nxt.pr]; }
-        } else { // font style logic -------------------------------------------
+        } else { // font style logic
           const fo = prop === 'bold' ? 1 : prop === 'dim' ? 2 : prop === 'normal' ? 22 : null;
           if (fo) {
             nxt.ft = [fo, 22, 0];
@@ -370,17 +231,19 @@ function createStain<C extends Record<string, number> = EmptyObject>(
       apply: (_target, _thisArg, args: string[]) => {
         let res = format(...args);
         // a loop here will dec perf by 6x
-        if (cur.re) { return escStain(res, 0, 0); }
-        if (cur.fg) { res = escStain(res, (cur.fg[2] ? '38;5;' : '') + cur.fg[0], cur.fg[1]); }
-        if (cur.bg) { res = escStain(res, (cur.bg[2] ? '48;5;' : '') + cur.bg[0], cur.bg[1]); }
-        if (cur.ft) { res = escStain(res, cur.ft[0], cur.ft[1], cur.ft[0]); }
-        if (cur.ue) { res = escStain(res, cur.ue[0], cur.ue[1]); }
-        if (cur.ie) { res = escStain(res, cur.ie[0], cur.ie[1]); }
+        if (cur.re) { return escFn(res, 0, 0); }
+        if (cur.fg) { res = escFn(res, (cur.fg[2] ? '38;5;' : '') + cur.fg[0], cur.fg[1]); }
+        if (cur.bg) { res = escFn(res, (cur.bg[2] ? '48;5;' : '') + cur.bg[0], cur.bg[1]); }
+        if (cur.ft) { res = escFn(res, cur.ft[0], cur.ft[1], cur.ft[0]); }
+        if (cur.ue) { res = escFn(res, cur.ue[0], cur.ue[1]); }
+        if (cur.ie) { res = escFn(res, cur.ie[0], cur.ie[1]); }
         return res;
       },
     });
-  return colorProxy() as CustomAnsiStainer<C, boolean>;
+  return colorProxy() as Stain<C, boolean>;
 }
+
+const stain = /* @__PURE__ */ (() => createStain({ xterm: true }))();
 
 const stain = createStain({ xterm: true });
 
